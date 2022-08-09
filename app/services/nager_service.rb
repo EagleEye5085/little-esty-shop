ced849116972c4eb0117af67720730f1f882a2a1
Thomas Turner <thomasturner482@yahoo.com>
2022-08-09 15:42:57 -0500

    add discount dependant destroy for merchant

--- app/services/nager_service.rb
+++ app/services/nager_service.rb
@@ -1,7 +1,6 @@
 class NagerService
 
   def get_url
-    binding.pry
     response = HTTParty.get("https://date.nager.at/api/v3/PublicHolidays/2022/US")
     JSON.parse(response.body, symbolize_names: true)
   end
